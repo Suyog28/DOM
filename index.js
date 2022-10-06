12607f8f7cb645646efb0e8fc153c2143c963e60
Suyog28 <suyogvmuley@gmail.com>
2022-10-06 17:18:57 +0530

    Using While Loop

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var output = [];
 
+// function fizzbuzz() {
+//   for (var count = 1; count < 101; count++) {
+//     if (count % 3 === 0 && count % 5 === 0) {
+//       output.push("FizzBuzz");
+//     } else if (count % 3 === 0) {
+//       output.push("Fizz");
+//     } else if (count % 5 === 0) {
+//       output.push("Buzz");
+//     } else {
+//       output.push(count);
+//     }
+//   }
+//   console.log(output);
+// }
+
+var count = 1;
 function fizzbuzz() {
-  for (var count = 1; count < 101; count++) {
+  while (count <= 101) {
     if (count % 3 === 0 && count % 5 === 0) {
       output.push("FizzBuzz");
     } else if (count % 3 === 0) {
@@ -11,16 +27,17 @@ function fizzbuzz() {
     } else {
       output.push(count);
     }
+    count++;
   }
   console.log(output);
 }
 
 fizzbuzz();
 
-var GuestList = ["Suyog", "Chaitali", "Mrunal", "Bhushan", "Pratik"];
-var GuestName = prompt("Your name is");
-if (GuestList.includes(GuestName)) {
-  alert("Welcome");
-} else {
-  alert("Sorry,Try next time");
-}
+// var GuestList = ["Suyog", "Chaitali", "Mrunal", "Bhushan", "Pratik"];
+// var GuestName = prompt("Your name is");
+// if (GuestList.includes(GuestName)) {
+//   alert("Welcome");
+// } else {
+//   alert("Sorry,Try next time");
+// }
